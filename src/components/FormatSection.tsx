b5bfd1657d6916cdcfac9d32e42826e6374b79e3
artartemev <79992180108@ya.ru>
2025-10-10 15:33:13 +0700

    Make PAMHO format section responsive

--- src/components/FormatSection.tsx
+++ src/components/FormatSection.tsx
@@ -1,6 +1,7 @@
 import nityanandaImage from '../assets/nityananda.png';
 import prabhupadaImage from '../assets/prabhupada.png';
 import { useLanguage } from '../contexts/LanguageContext';
+import styles from './FormatSection.module.css';
 
 const translations = {
   ru: {
@@ -29,119 +30,22 @@ export function FormatSection() {
     translations[language];
 
   return (
-    <section
-      style={{
-        backgroundColor: '#E9E5E2',
-        display: 'flex',
-        justifyContent: 'center'
-      }}
-    >
-      <div
-        style={{
-          position: 'relative',
-          width: '1440px',
-          height: '1524px'
-        }}
-      >
-        <h2
-          style={{
-            position: 'absolute',
-            left: '100px',
-            top: '80px',
-            width: '1222px',
-            height: '101px',
-            fontFamily: 'MenorahGrotesk',
-            fontStyle: 'normal',
-            fontWeight: 500,
-            fontSize: '84px',
-            lineHeight: '101px',
-            color: '#000000'
-          }}
-        >
-          {heading}
-        </h2>
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>{heading}</h2>
 
-        <p
-          style={{
-            position: 'absolute',
-            left: '580px',
-            top: '221px',
-            width: '743px',
-            height: '660px',
-            fontFamily: 'MenorahGrotesk',
-            fontStyle: 'normal',
-            fontWeight: 500,
-            fontSize: '24px',
-            lineHeight: '36px',
-            color: '#000000'
-          }}
-        >
-          {firstParagraph}
-        </p>
-
-        <p
-          style={{
-            position: 'absolute',
-            left: '149px',
-            top: '963px',
-            width: '713px',
-            height: '428px',
-            fontFamily: 'MenorahGrotesk',
-            fontStyle: 'normal',
-            fontWeight: 500,
-            fontSize: '24px',
-            lineHeight: '36px',
-            textAlign: 'right',
-            color: '#000000'
-          }}
-        >
-          {secondParagraph}
-        </p>
-
-        <div
-          style={{
-            position: 'absolute',
-            left: '120px',
-            top: '225px',
-            width: '396px',
-            height: '652px',
-            borderRadius: '180px',
-            overflow: 'hidden'
-          }}
-        >
-          <img
-            src={nityanandaImage}
-            alt={nityanandaAlt}
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              objectPosition: 'center'
-            }}
-          />
+        <div className={styles.topRow}>
+          <div className={styles.imageTall}>
+            <img src={nityanandaImage} alt={nityanandaAlt} />
+          </div>
+          <p className={styles.firstParagraph}>{firstParagraph}</p>
         </div>
 
-        <div
-          style={{
-            position: 'absolute',
-            left: '926px',
-            top: '932px',
-            width: '350px',
-            height: '489px',
-            borderRadius: '180px',
-            overflow: 'hidden'
-          }}
-        >
-          <img
-            src={prabhupadaImage}
-            alt={prabhupadaAlt}
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'contain',
-              objectPosition: 'center'
-            }}
-          />
+        <div className={styles.bottomRow}>
+          <p className={styles.secondParagraph}>{secondParagraph}</p>
+          <div className={styles.imageWide}>
+            <img src={prabhupadaImage} alt={prabhupadaAlt} />
+          </div>
         </div>
       </div>
     </section>
